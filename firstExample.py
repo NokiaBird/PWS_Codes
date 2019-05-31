b1e36d525cbcf78c0730908f83bb72c1ac13bad5
Nokia Bird <WillyNillyy@protonmail.com>
2019-05-31 01:40:45 -0700

    added boolean examples

---
+++ firstExample.py
@@ -0,0 +1,8 @@
+print("Who are you here to watch boouy?")
+name = input()
+if name == "Arsenal":
+    print("You people are useless")
+elif name == "Real Madrid":
+    print("Corruption days are over we have VAR hhh")
+else:
+    print("Be quiet")
