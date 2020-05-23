2d4727e99386b65f4fea8a876c1b2d02f41d039b
Nokia Bird <WillyNillyy@protonmail.com>
2020-05-23 09:55:24 -0400

    I'm just trying to avoid paying beeminder for being lazy but this isn't real code, but hey this maybe my way rolling the barrel

---
+++ caajis.py
@@ -0,0 +1,1 @@
+print("Niyohoow hurdo xun a iheeso lkn beeminder aa lacagteyda cunaayo hdn commitkaan suubinin :(")
--- firstgame.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.init()
-
-WIDTH = 800
-HEIGHT = 600
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
