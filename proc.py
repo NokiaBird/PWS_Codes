55d50906fbdf9b32fa85367680b9e9109a4a3af8
Nokia Bird <WillyNillyy@protonmail.com>
2020-06-02 16:29:05 -0400

    this time i'll finish this code properly tomorrow in sha allah

---
+++ proc.py
@@ -0,0 +1,9 @@
+import random
+
+from random import randint
+random_number = random.randint(1,15)
+
+while True:
+	guess = input("Pick a number between 1 and 15")
+	guess = int(guess)
+	# Will finish this tomorrow in sha allah
