c21f470944e26628a723105fc9722f040de6a386
Nokia Bird <WillyNillyy@protonmail.com>
2020-06-27 22:24:41 -0400

    pushing real code for the first time in a year

---
+++ first_function.py
@@ -0,0 +1,7 @@
+def sing_happy_birthday():
+	print("Happy birthday To You")
+	print("Happy birthday To You")
+	print("Happy birthday dear You")
+	print("Happy birthday To You")
+
+sing_happy_birthday()
