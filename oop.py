0f06832da2f1449c45ee3ad52f1e692ee36c6470
Nokia Bird <WillyNillyy@protonmail.com>
2020-09-21 23:56:10 -0400

    tomorrow code

--- oop.py
+++ oop.py
@@ -1,6 +1,6 @@
 # Python Object-Oriented Programming
 # Starting the reddit syllabus for Python 
-def hello_world():
+def hello_world(word):
 	print("Hello from a function")
 hello_world()
 #Here goes the next line tomorrow in sha allah
